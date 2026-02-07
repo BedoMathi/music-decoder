a2ca585a49ef914c517bb4a6f9d953842087547e
Rishav <sarkarrishav85@gmail.com>
2026-02-08 04:19:23 +0530

    blank page fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/MUSIC-DECODER/",
+  base: "/music-decoder/",
   build: {
     outDir: "../docs",
     emptyOutDir: true
